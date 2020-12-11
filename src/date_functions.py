4d7fb1217c1092d70c19a13cf97601890e024749
EMCain <>
2020-12-10 20:00:46 -0800

    add third tuesday function

--- src/date_functions.py
+++ src/date_functions.py
@@ -8,6 +8,15 @@ def first_sunday(year, month):
     first_sunday = calendar_month[0][6]
     return datetime.date(year=year, month=month, day=first_sunday)
 
+def third_tuesday(year, month): 
+    # make the first weekday Wednesday (Monday = 0, Tuesday = 1...)
+    cal = calendar.Calendar(firstweekday=2)
+    calendar_month = cal.monthdayscalendar(year=year, month=month)
+    # Tuesday will be the last day of the week, so we can always use the third week. 
+    third_week = calendar_month[2]
+    third_tuesday = third_week[6]
+    return datetime.date(year=year, month=month, day=third_tuesday)
+
 
 def this_year_info():
     year = datetime.date.today().year
