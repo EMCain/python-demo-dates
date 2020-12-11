df3c7dd0b3b05c2c202bb1260f79c16059ea1376
EMCain <>
2020-12-10 21:04:25 -0800

    Test third_tuesday

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -3,7 +3,7 @@ import datetime
 import pytest
 from freezegun import freeze_time
 
-from src.date_functions import first_sunday, this_year_info
+from src.date_functions import first_sunday, third_tuesday, this_year_info
 
 
 @pytest.mark.parametrize(
@@ -18,6 +18,22 @@ from src.date_functions import first_sunday, this_year_info
 )
 def test_first_sunday_returns_the_first_sunday_of_the_month(year, month, expected):
     result = first_sunday(year, month)
+    assert result == datetime.datetime.strptime(expected, "%Y-%m-%d").date()
+
+
+
+@pytest.mark.parametrize(
+    "year,month,expected",
+    [
+        (2002, 2, "2002-02-19"),
+        (2006, 5, "2006-05-16"),
+        (2020, 11, "2020-11-17"),
+        (2029, 7, "2029-07-17"),
+        (2036, 6, "2036-06-17"),
+    ]
+)
+def test_third_tuesday_returns_the_third_tuesday_of_the_month(year, month, expected):
+    result = third_tuesday(year, month)
     assert result == datetime.datetime.strptime(expected, "%Y-%m-%d").date()
 
 
